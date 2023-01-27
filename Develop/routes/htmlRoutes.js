0f0b87a5110a3f130b863a31ca2f614a61820ed2
hynookim98 <hynookim98@gmail.com>
2023-01-27 10:51:44 -0800

    added routes to return HTMLS

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const router = require('express').Router();
+
+// get notes route to return the notes.html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'notes.html'));
+});
+
+// get * route to return the index.html file
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
